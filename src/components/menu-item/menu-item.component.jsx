adad7af4828f0b5b7f29e1a64d102e7016179fc0
Marisol Melendres <marisolmelendresm@gmail.com>
2022-01-21 11:37:24 -0600

    navigation using react router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, img, size }) => (
-    <div className = {`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className = {`${size} menu-item`} onClick = {() => history.push(`${match.url}${linkUrl}`)}>
         <div className = 'background-image' style = {{
-            backgroundImage: `url(${img})`
+            backgroundImage: `url(${imageUrl})`
         }} />
         <div className = 'content'>
             <h1 className = 'title'>{title.toUpperCase()}</h1>
@@ -14,4 +15,4 @@ const MenuItem = ({ title, img, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
